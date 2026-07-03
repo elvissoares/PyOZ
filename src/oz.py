2bc8f6dda0510443b8c2b4a4fd8666dc5e05b771
Elvis Soares <elvis.asoares@gmail.com>
2026-07-03 10:45:22 -0300

    Updating examples

--- src/oz.py
+++ src/oz.py
@@ -35,7 +35,7 @@ class PyOZ:
             b_r = 0.0
         return torch.exp(-beta_u + gamma + b_r) - gamma - 1
 
-    def solve(self, rho: float, kBT: float = 1.0, rmax: float = 5.0, dr: float = 0.01, max_iter: int = 1000, method: str = 'Anderson', mix_depth: int = 5, alpha: float = 0.6, atol: float = 1e-8, logoutput: bool = False) -> Tuple[np.ndarray, np.ndarray]:
+    def solve(self, rho: float, kBT: float = 1.0, rmax: float = 5.0, dr: float = 0.01, max_iter: int = 1000, method: str = 'Anderson', mix_depth: int = 5, alpha: float = 0.6, atol: float = 1e-6, logoutput: bool = False) -> Tuple[np.ndarray, np.ndarray]:
         """
         Solve the Ornstein-Zernike equation for a given radial grid r.
         Returns (r, h_r, c_r).
@@ -50,7 +50,9 @@ class PyOZ:
         k = (i + 0.5) * dk
 
         # initial outer functions
-        gamma_r = torch.zeros_like(r, device=self.device)
+        c_r = torch.zeros_like(r, device=self.device)
+        h_r = torch.zeros_like(r, device=self.device)
+        gamma_r = h_r - c_r  # indirect correlation function
 
         # Anderson mixer
         if method == 'Anderson':
@@ -60,18 +62,18 @@ class PyOZ:
 
         for it in range(1, max_iter + 1):
             # closure
-            c_r = self._closure_relation(gamma_r, r)
+            c_r[:] = self._closure_relation(gamma_r, r)
             # OZ in k-space
             c_k = hankel_forward(c_r, r, k, dr)
             # test if rho*ck is smaller than 1 to avoid divergence using clamp
             denominator = 1.0 / (1.0 - self.rho * c_k)
-            denominator = torch.clamp(denominator, min=0.0, max=1e9)
+            denominator = torch.clamp(denominator, min=0.0, max=1e9)  # avoid divergence
             gamma_k = self.rho * c_k * c_k  * denominator
             # back to r-space
             gamma_new = hankel_inverse(gamma_k, r, k, dk)  # inverse DST-I normalization
 
             # testing convergence
-            residue = gamma_new - gamma_r
+            residue = (gamma_new - gamma_r)*r
             err = torch.sqrt(dr*torch.pow(residue, 2.0).sum()) / atol
 
             if logoutput and it % 100 == 0:
@@ -84,6 +86,6 @@ class PyOZ:
 
         self.Niter = it # store the number of iterations
         
-        c_r = self._closure_relation(gamma_r, r)
-        h_r = gamma_r + c_r
+        c_r[:] = self._closure_relation(gamma_r, r)
+        h_r[:] = gamma_r + c_r
         return r, h_r, c_r
